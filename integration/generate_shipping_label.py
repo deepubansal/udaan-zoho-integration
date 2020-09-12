f8319e9f231b5044b32b7865ae38b41a8966be7e
deepbans <deepbans@adobe.com>
2020-09-13 00:43:52 +0530

    shipping label file name include invoice number now

--- integration/generate_shipping_label.py
+++ integration/generate_shipping_label.py
@@ -4,7 +4,7 @@ from integration.udaan.orders.upload_rts import upload_and_start_shipping_label_
 
 def generate(invoice_number):
     output_file = generate_files(invoice_number)
-    pdf_path = upload_and_start_shipping_label_job(output_file)
+    pdf_path = upload_and_start_shipping_label_job(output_file, invoice_id=invoice_number)
     print("File downloaded at path: file://{}".format(pdf_path))
     return pdf_path
 
