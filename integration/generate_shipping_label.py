f34d61772c8b43e5279a21bcd239989f3a665ef4
deepbans <deepbans@adobe.com>
2020-09-20 14:02:02 +0530

    shipping label file name include invoice number now

--- integration/generate_shipping_label.py
+++ integration/generate_shipping_label.py
@@ -6,6 +6,6 @@ def generate(invoice_number):
     output_file = generate_files(invoice_number)
     pdf_path = upload_and_start_shipping_label_job(output_file, invoice_id=invoice_number)
     print("File downloaded at path: file://{}".format(pdf_path))
-    return pdf_path
+    return pdf_path, output_file
 
 
